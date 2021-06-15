cafbb3fe0355c08dd04a0879fc5e3c9b2af5b5da
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-06-15 17:05:55 -0500

    add comments to contract

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,23 +1,27 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.7.0;
+pragma solidity ^0.8.3;
 
+/// @dev Allows for console.log in Solidity
 import "hardhat/console.sol";
 
-
+/// @notice Create a contract and initialize a state variable
 contract Greeter {
-  string greeting;
+    string greeting;
 
-  constructor(string memory _greeting) {
-    console.log("Deploying a Greeter with greeting:", _greeting);
-    greeting = _greeting;
-  }
+    /// @dev Get the state variable and pass in as argument when contract is first called
+    constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:", _greeting);
+        greeting = _greeting;
+    }
 
-  function greet() public view returns (string memory) {
-    return greeting;
-  }
+    /// @dev Returns state vaiable getting, can only view and allows external access
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
 
-  function setGreeting(string memory _greeting) public {
-    console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-    greeting = _greeting;
-  }
+    /// @dev Allows external access to change the greeting variable
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
 }
